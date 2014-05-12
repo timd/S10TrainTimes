7f2d300e52e083ba0e9ab20f9296a0e1d6b33958
Tim Duckett <tim.duckett@centralway.com>
2014-05-12 13:29:10 +0200

    Initial commit

--- NetworkStubs/ViewController.m
+++ NetworkStubs/ViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
+#import "OHHTTPStubs.h"
 
 @interface ViewController ()
-
+@property (nonatomic, weak) IBOutlet UIButton *downloadButton;
+@property (nonatomic, weak) IBOutlet UILabel *arrivalTime;
+@property (nonatomic, weak) IBOutlet UILabel *departureTime;
+@property (nonatomic, weak) IBOutlet UIActivityIndicatorView *departureSpinner;
+@property (nonatomic, weak) IBOutlet UIActivityIndicatorView *arrivalSpinner;
+@property (nonatomic, weak) NSTimer *spinnerTimer;
 @end
 
 @implementation ViewController
@@ -18,12 +25,92 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.path isEqualToString:@"/v1/connections"];
+    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+        return [[OHHTTPStubsResponse responseWithFileAtPath:OHPathForFileInBundle(@"response.json",nil)
+                                                 statusCode:200 headers:@{@"Content-Type":@"text/json"}]
+                requestTime:4.0 responseTime:1.0];
+    }];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)didTapDownloadButton:(id)sender {
+    
+    NSURL *url = [NSURL URLWithString:@"http://transport.opendata.ch/v1/connections?from=008503055&to=008503051&limit=1"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        NSDictionary *responseDictionary = (NSDictionary *)responseObject;
+        
+        NSArray *connections = [responseDictionary objectForKey:@"connections"];
+        
+        NSDictionary *resultsDictionary = [connections objectAtIndex:0];
+        
+        NSDictionary *fromData = [resultsDictionary objectForKey:@"from"];
+        NSDictionary *toData = [resultsDictionary objectForKey:@"to"];
+
+        NSDateFormatter *longFormatter = [[NSDateFormatter alloc] init];
+        [longFormatter setDateFormat:@"YYYY-MM-dd'T'HH:mm:ssZZZZZ"];            // 2014-05-11T18:53:00+0200
+
+        NSDateFormatter *shortFormatter = [[NSDateFormatter alloc] init];
+        [shortFormatter setDateFormat:@"HH:mm"];
+        
+        NSString *departureTime = [fromData objectForKey:@"departure"];
+        NSString *arrivalTime = [toData objectForKey:@"arrival"];
+
+        NSDate *depTime = [longFormatter dateFromString:departureTime];
+        NSDate *arrTime = [longFormatter dateFromString:arrivalTime];
+        
+        NSString *cleanArrTime = [shortFormatter stringFromDate:arrTime];
+        NSString *cleanDepTime = [shortFormatter stringFromDate:depTime];
+      
+        [self.departureSpinner setAlpha:0.0f];
+        [self.arrivalSpinner setAlpha:0.0f];
+        [self.departureSpinner stopAnimating];
+        [self.arrivalSpinner stopAnimating];
+        
+        [self.departureTime setText:cleanDepTime];
+        [self.arrivalTime setText:cleanArrTime];
+
+        [self.spinnerTimer invalidate];
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        NSLog(@"Failed :(");
+        
+    }];
+    
+    [operation start];
+    
+    [self.departureTime setText:@""];
+    [self.arrivalTime setText:@""];
+    
+    [self.departureSpinner setAlpha:1.0f];
+    [self.arrivalSpinner setAlpha:1.0f];
+    [self.departureSpinner startAnimating];
+    [self.arrivalSpinner startAnimating];
+    
+    self.spinnerTimer = [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(changeSpinnerColour) userInfo:nil repeats:NO];
+    
+}
+
+-(void)changeSpinnerColour {
+    
+    [self.arrivalSpinner setColor:[UIColor redColor]];
+    [self.departureSpinner setColor:[UIColor redColor]];
+    
 }
 
 @end
---
+++ README.md
@@ -0,0 +1,21 @@
+### A Cowardly Test-o-phobe's Guide To Test-Driven Development With iOS
+
+#### Being an example from a presentation given at iOSCon London, May 2014
+
+This example demonstrates the use of OHHTTPStubs to simulate a network connection.
+
+The app puports to retrieve the time of the next S10 train between Uitikon Waldegg and Zürich Hauptbahnhof.  In normal circumstances, this data would be available through the magic of the transport.opendata.ch API.
+
+In this example, the network request is mocked out by OHHTTPStubs, and loads the data from a JSON file in the app bundle.  It also simulates a slow network response to force the activity indicator to turn red (indicating a long-running request).
+
+#### Prerequistes
+
+1. Xcode 5.1 or later
+1. [Kiwi](https://github.com/allending/Kiwi)
+1. [OHHTTPStubs](https://github.com/AliSoftware/OHHTTPStubs)
+
+### Licensing
+
+This project is licensed under Creative Commons Attribution NonCommercial ShareAlike 3.0 Unported terms.  Feel free to use noncommercially as you see fit; if you include it in teaching materials or similar, then a link back to my site [http://adoptioncurve.net](http://adoptioncurve.net) would be appreciated.
+
+Copyright (cc) 2013 Tim Duckett
